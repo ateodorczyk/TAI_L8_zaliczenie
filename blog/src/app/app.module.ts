b552fb699ef36293c097d096dc3dfc344d1f9d63
ateodorczyk <28362@student.pwsztar.edu.pl>
2018-10-25 00:39:35 +0200

    angular

---
+++ blog/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
